0a982bed8bd092ac9d34ee5e0911cf726aab2ea5
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2021-04-09 12:11:38 +0100

    Add files via upload

--- aotp/views.py
+++ aotp/views.py
@@ -6,12 +6,17 @@ import sys
 from subprocess import run, PIPE
 from . import functions
 
+from ratelimit.decorators import ratelimit
+
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+
+@ratelimit(key='ip', rate='1/2m', method=ratelimit.ALL, block=True)
 def external(request):
     img_data = request.POST.get('img_data')
     artist = request.POST.get('artist')
     title = request.POST.get('title')
-    functions.post_img(img_data, title, artist)
+    print("*"*50)
+    # functions.post_img(img_data, title, artist)
     return render(request, 'home.html')
