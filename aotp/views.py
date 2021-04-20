9fef7bb751882a078e8ec61c418623cc2f0bb642
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2021-04-20 13:21:26 +0100

    Update views.py

--- aotp/views.py
+++ aotp/views.py
@@ -10,12 +10,12 @@ from ratelimit.decorators import ratelimit
 
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-@ensure_csrf_cookie
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 
 @ratelimit(key='ip', rate='1/2m', method=ratelimit.ALL, block=True)
+@ensure_csrf_cookie
 def external(request):
     img_data = request.POST.get('img_data')
     artist = request.POST.get('artist')
